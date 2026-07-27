ed7332080e6a4457afbad288efbabc485689fc6e
nourabosaif <abosaif.nany@gmail.com>
2026-07-27 17:43:46 +0300

    error fixed

--- airflow/sql/bronze/res_partner.sql
+++ airflow/sql/bronze/res_partner.sql
@@ -3,17 +3,17 @@ CREATE TABLE IF NOT EXISTS nour_bronze.res_partner
     id UInt64,
     name String,
     email String null,
-    phone String,
-    street String,
-    city String,
-    city_id UInt32,
-    muhafaza_id UInt32,
-    is_company Bool,
-    partner_type String,
-    segment String,
+    phone String null,
+    street String null,
+    city String null,
+    city_id UInt32 null,
+    muhafaza_id UInt32 null,
+    is_company Bool ,
+    partner_type String null,
+    segment String null,
     active Bool,
-    create_date DateTime64(3),
-    write_date DateTime64(3)
+    create_date DateTime64(3) null,
+    write_date DateTime64(3) null
 )
 ENGINE = MergeTree
-ORDER BY id;
+ORDER BY id

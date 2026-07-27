312a57cbd80cbf0b2d047531e106fb27b37a8b65
nourabosaif <abosaif.nany@gmail.com>
2026-07-27 16:12:16 +0300

    a testing dag

--- airflow/dags/bronze_sales.py
+++ airflow/dags/bronze_sales.py
@@ -12,9 +12,9 @@ def get_clickhouse_client():
     return get_client(
         host="clickhouse",
         port=8123,
-        username="default",
-        password="",
-        database="default",
+        username="dataworm",
+        password="datawaorm",
+        database="warehouse",
     )
 
 def execute_sql_file(sql_file_path: str):

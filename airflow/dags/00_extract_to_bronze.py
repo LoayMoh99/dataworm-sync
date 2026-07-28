fc952fd6663d41b9fe192d633cfa1581b488851d
salahabdullah214-commits <salahabdullah214@gmail.com>
2026-07-28 15:07:04 +0300

    feat: updated bronze dag to use salah_bronze

--- airflow/dags/00_extract_to_bronze.py
+++ airflow/dags/00_extract_to_bronze.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 import pandas as pd
 from sqlalchemy import create_engine
 
+# 1. الإعدادات الافتراضية للـ DAG
 default_args = {
     'owner': 'dataworm',
     'depends_on_past': False,
@@ -14,15 +15,16 @@ default_args = {
 }
 
 
+# 2. الدالة الأساسية لاستخراج البيانات ونقلها لـ ClickHouse
 def extract_postgres_to_bronze(table_name):
-    # 1. القراءة من PostgreSQL
+    # الاتصال بقاعدة بيانات postgres وقراءة الجدول
     pg_engine = create_engine(
         'postgresql://readonly_user:000@postgres:5432/warehouse'
     )
     query = f'SELECT * FROM {table_name}'
     df = pd.read_sql(query, pg_engine)
 
-    # 2. الاتصال بـ ClickHouse
+    # الاتصال بـ ClickHouse
     ch_client = clickhouse_connect.get_client(
         host='clickhouse',
         port=8123,
@@ -30,20 +32,21 @@ def extract_postgres_to_bronze(table_name):
         password='dataworm',
     )
 
-    # 3. إنشاء قاعدة البيانات
-    ch_client.command('CREATE DATABASE IF NOT EXISTS bronze')
-
+    db_name = 'salah_bronze'
     target_table = f'raw_{table_name}'
 
-    # 4. تحويل أنواع Pandas لدوال ClickHouse وإنشاء الجدول بـ SQL مباشر
-    # insert_df ستقوم بإنشاء الجدول تلقائياً أو إدخال البيانات إذا كان الجدول غير موجود بشرط عدم وجود settings خاطئة
+    # التأكد من وجود داتابيز salah_bronze
+    ch_client.command(f'CREATE DATABASE IF NOT EXISTS {db_name}')
+
+    # إدخال البيانات في الجدول المجهز
     ch_client.insert_df(
         table=target_table,
         df=df,
-        database='bronze'
+        database=db_name
     )
 
 
+# 3. تعريف الـ DAG
 with DAG(
     '00_extract_to_bronze',
     default_args=default_args,
